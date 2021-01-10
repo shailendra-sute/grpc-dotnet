38ae1077f9c2eb680049216c5c7eb693558bd79d
Shailendra Sute <shailendra.sute@hotmail.com>
2021-01-10 17:38:22 -0600

    Example gRPC Greetings and Bookstore

---
+++ src/ExampleGrpc/ExampleClientApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExampleClient;
+using Grpc.Net.Client;
+using System;
+
+namespace ExampleClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            var bookstoreClient = new Bookstore.BookstoreClient(channel);
+            var shelf = bookstoreClient.GetShelf(new GetShelfRequest { Shelf = 878788787 });
+            Console.WriteLine($"Shelf Response Id: {shelf.Id} and Theme: {shelf.Theme}");
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ src/ExampleGrpc/ExampleGrpcService/Services/BookstoreService.cs
@@ -0,0 +1,27 @@
+﻿using ExampleGrpcService.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ExampleGrpcService
+{
+    public class BookstoreService : Bookstore.BookstoreBase
+    {
+        private readonly ILogger<BookstoreService> _logger;
+        public BookstoreService(ILogger<BookstoreService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<Shelf> GetShelf(GetShelfRequest request,
+            ServerCallContext context)
+        {
+            _logger.LogInformation("BookService called with Create Shelf Request: {0}", request);
+            return await Task.FromResult(new Shelf
+            {
+                Id = request.Shelf,
+                Theme = "Non Fiction"
+            });
+        }
+    }
+}
